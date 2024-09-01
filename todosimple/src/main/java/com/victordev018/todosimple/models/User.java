5a28530cbee053767ef66e27100b48c603eed917
João VIctor <joawrt8520@gmail.com>
2024-09-01 15:20:36 -0300

    feat: add lombok dependecy

--- todosimple/src/main/java/com/victordev018/todosimple/models/User.java
+++ todosimple/src/main/java/com/victordev018/todosimple/models/User.java
@@ -1,19 +1,23 @@
 package com.victordev018.todosimple.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 
 @Entity     // treats it as an entity ready to make a CRUD
 @Table(name = User.TABLE_NAME)  // treat with a table of the database
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
 public class User {
 
     // validation groups
@@ -44,74 +48,7 @@ public class User {
     private String password;
 
     @OneToMany(mappedBy = "user")   // mappedBy variable user from Task
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)  // no return list task, only write
     private List<Task> tasks = new ArrayList<>();
 
-    // constructors
-    public User(){
-    }
-
-    public User(Long id, String username, String password){
-        this.id = id;
-        this.username = username;
-        this.password = password;
-    }
-
-    // getters and setters
-    public Long getId(){
-        return id;
-    }
-
-    public void setId(Long id){this.id = id;}
-
-    public String getUsername(){
-        return username;
-    }
-
-    public void setUsername(String username){
-        this.username = username;
-    }
-
-    public @NotNull(groups = {CreateUser.class, UpdateUser.class}) @NotEmpty(groups = {CreateUser.class, UpdateUser.class}) @Size(min = 8, max = 60, groups = {CreateUser.class, UpdateUser.class}) String getPassword() {
-        return password;
-    }
-
-    public void setPassword(@NotNull(groups = {CreateUser.class, UpdateUser.class}) @NotEmpty(groups = {CreateUser.class, UpdateUser.class}) @Size(min = 8, max = 60, groups = {CreateUser.class, UpdateUser.class}) String password) {
-        this.password = password;
-    }
-
-    @JsonIgnore
-    public List<Task> getTasks(){
-        return tasks;
-    }
-
-    // hashCode and equals
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-
-        if (!(obj instanceof User))
-            return false;
-
-        User user = (User) obj;
-        if (this.id == null) {
-            if (((User) obj).id != null){
-                return false;
-            } else if (!    this.id.equals(user.id)) {
-                return false;
-            }
-        }
-        return Objects.equals(id, user.id) && Objects.equals(username, user.username) &&
-                Objects.equals(password, user.password);
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null)? 0 : id.hashCode());
-        return result;
-    }
 }
