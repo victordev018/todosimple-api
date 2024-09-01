54fecf4b8cf431c115ef92236a1c992e3d58ed00
João VIctor <joawrt8520@gmail.com>
2024-09-01 00:23:45 -0300

    feat: create WebConfig

---
+++ todosimple/src/main/java/com/victordev018/todosimple/configs/WebConfig.java
@@ -0,0 +1,17 @@
+package com.victordev018.todosimple.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**");     // any mapping will have access to the routes
+    }
+
+}
